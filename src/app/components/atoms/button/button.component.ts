03222f800420d85778b1300be63be76486ac53c8
HiddenUndefined <georgerudenko97@gmail.com>
2023-10-03 16:36:12 +0300

    feat: 🚀 added a new input and binding the attr for the button element

--- src/app/components/atoms/button/button.component.ts
+++ src/app/components/atoms/button/button.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core'
+import { Component, HostBinding, Input } from '@angular/core'
+
+/**
+ * Button types *(only for button color schema)*
+ */
+type TButtonTypes = 'danger'
 
 @Component({
   selector: 'button[eg-button]',
@@ -7,4 +12,6 @@ import { Component } from '@angular/core'
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent {
+  @HostBinding('attr.btn-style') @Input()
+  colorType?: TButtonTypes
 }
