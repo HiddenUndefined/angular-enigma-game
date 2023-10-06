5ba0c5d64c231752c453a86c3041a1abf195747f
HiddenUndefined <georgerudenko97@gmail.com>
2023-10-06 22:12:47 +0300

    feat: 🚀 created the present organism component and used them in the home page

--- src/app/components/organisms/index.ts
+++ src/app/components/organisms/index.ts
@@ -1,1 +1,3 @@
 export * from './header'
+export * from './game-header'
+export * from './present'
---
+++ src/app/components/organisms/present/present.component.ts
@@ -0,0 +1,22 @@
+import { Component, HostBinding, Input } from '@angular/core'
+import { RouterLink } from '@angular/router'
+
+@Component({
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  selector: 'eg-organism-present',
+  templateUrl: './present.component.html',
+  styleUrls: ['./present.component.css']
+})
+export class PresentComponent {
+  // @Host
+  @HostBinding('class') classes = 'd-flex fd-column ai-center jc-center'
+
+  // @Properties
+  @Input() header = 'Block header'
+  @Input() description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet cupiditate non, quisquam veritatis vitae voluptatibus!'
+  @Input() linkHref = '#'
+  @Input() linkText = 'Link text'
+}
